bbc5d30f1c60c5a74eb1ca635f0e8ab827896a1f
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-20 10:16:23 -0400

    first part

--- author.rb
+++ author.rb
@@ -1,1 +1,31 @@
+class Author
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        Author.all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select {|book| book.author == self}
+    end
+
+    def write_book(title, word_count)
+        book = Book.new(self, title, word_count)
+    end
+
+    def total_words
+        counts = self.books.map {|book| book.word_count}
+        counts.sum
+    end
+
+    def self.most_words
+        all_totals = Author.all.map {|author| author.total_words}
+        max = all_totals.max {|count1, count2| count1 <=> count2}
+        Author.all.find {|author| author.total_words == max}
+    end
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,17 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+s_collins = Author.new("Suzanne Collins")
+f_dostoyevsky = Author.new("Fyodor Dostoyevsky")
+j_steinbeck = Author.new("John Steinbeck")
+
+hunger_games = Book.new(s_collins, "The Hunger Games", 50000)
+the_idiot = Book.new(f_dostoyevsky, "The Idiot", 100000)
+crime_and_punishment = Book.new(f_dostoyevsky, "Crime and Punishment", 90000)
+grapes_of_wrath = Book.new(j_steinbeck, "The Grapes of Wrath", 75000)
+
+
 binding.pry
+
 
 
